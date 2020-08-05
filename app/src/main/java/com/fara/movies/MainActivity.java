dad828b5e3ec5262686b6e845114d31a9e47e8dd
faa0 <wa3rix@gmail.com>
2020-08-05 17:42:35 +0300

    Added DB, smth visual things

--- app/src/main/java/com/fara/movies/MainActivity.java
+++ app/src/main/java/com/fara/movies/MainActivity.java
@@ -1,11 +1,22 @@
 package com.fara.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.fara.movies.data.MainViewModel;
 import com.fara.movies.data.Movie;
 import com.fara.movies.utils.JSONUtils;
 import com.fara.movies.utils.NetworkUtils;
@@ -15,25 +26,104 @@ import org.json.JSONObject;
 
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch switchSort;
+    private RecyclerView rvPosters;
+    private MovieAdapter movieAdapter;
+    private TextView tvTopRated;
+    private TextView tvPopularity;
+
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvTopRated = findViewById(R.id.tvTopRated);
+        tvPopularity = findViewById(R.id.tvPopularity);
+        switchSort = findViewById(R.id.switchSort);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        rvPosters = findViewById(R.id.rvPosters);
+        rvPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
+        rvPosters.setAdapter(movieAdapter);
+
+        switchSort.setChecked(true);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                setMethodOfSort(b);
+            }
+        });
+        switchSort.setChecked(false);
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this, "sos", Toast.LENGTH_SHORT).show();
+            }
+        });
+        LiveData<List<Movie>> moviesFromLiveData = viewModel.getMovies();
+        moviesFromLiveData.observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                movieAdapter.setMovies(movies);
+            }
+        });
+    }
+
+    public void onClickSetPopularity(View view) {
+        setMethodOfSort(false);
+        switchSort.setChecked(false);
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethodOfSort(true);
+        switchSort.setChecked(true);
+    }
+
+    public void setMethodOfSort(boolean isTopRated) {
+        int methodOfSort;
+        if (isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            tvTopRated.setTextColor(getResources().getColor(R.color.colorAccent));
+            tvPopularity.setTextColor(getResources().getColor(R.color.white));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+            tvPopularity.setTextColor(getResources().getColor(R.color.colorAccent));
+            tvTopRated.setTextColor(getResources().getColor(R.color.white));
+        }
+        downloadDate(methodOfSort, 1);
+    }
+
+    private void downloadDate(int methodOfSort, int page) {
+
         JSONObject jsonObject = null;
         try {
-            jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+            jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
+        } catch (MalformedURLException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
             ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-            StringBuilder builder = new StringBuilder();
-            for (Movie movie : movies) {
-                builder.append(movie.getTitle()).append("\n");
+            if (movies != null && !movies.isEmpty()) {
+                viewModel.deleteAllMovies();
+                for(Movie movie : movies) {
+                    viewModel.insertMovie(movie);
+                }
             }
-            Log.i("MyResult", builder.toString());
-        } catch (MalformedURLException | ExecutionException | InterruptedException | JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
--- app/src/main/java/com/fara/movies/data/Movie.java
+++ app/src/main/java/com/fara/movies/data/Movie.java
@@ -1,27 +1,41 @@
 package com.fara.movies.data;
 
-public class Movie {
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "movies")
+public class Movie {
+    @PrimaryKey
     private int id;
     private int voteCount;
     private String title;
     private String originalTitle;
-    private String overview;;
+    private String overview;
     private String posterPath;
+    private String bigPosterPath;
     private String backdropPath;
     private double voteAverage;
     private String releaseDate;
 
-    public Movie(int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String backdropPath, double voteAverage, String releaseDate) {
+    public Movie(int id, int voteCount, String title, String originalTitle, String overview, String posterPath, String bigPosterPath, String backdropPath, double voteAverage, String releaseDate) {
         this.id = id;
         this.voteCount = voteCount;
         this.title = title;
         this.originalTitle = originalTitle;
         this.overview = overview;
         this.posterPath = posterPath;
+        this.bigPosterPath = bigPosterPath;
         this.backdropPath = backdropPath;
         this.voteAverage = voteAverage;
         this.releaseDate = releaseDate;
+    }
+
+    public String getBigPosterPath() {
+        return bigPosterPath;
+    }
+
+    public void setBigPosterPath(String bigPosterPath) {
+        this.bigPosterPath = bigPosterPath;
     }
 
     public int getId() {
